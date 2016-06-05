e4dbc98fad7eb1b138f67330db4f55e06ade0535
mattia richetto <mattia.richetto@gmail.com>
2016-06-05 19:59:39 +0100

    add images and home section

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('page:', req.query.page);
+  var page = parseInt(req.query.page);
+  res.render('index', { title: 'Express', page: page });
 });
 
 module.exports = router;

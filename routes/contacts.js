8bd7f5d4efb0632852bacd5f4b292587d6f6a33a
mattia richetto <mattia.richetto@gmail.com>
2016-06-05 20:10:10 +0100

    add contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  var page = parseInt(req.query.page);
-  var model = require('./model');
-  model.page = page;
-  res.render('index', model);
+  res.render('contacts', require('./model'));
 });
 
 module.exports = router;
---
+++ routes/model.js
@@ -0,0 +1,1 @@
+module.exports = { title: 'Il Nido dei Monelli' };

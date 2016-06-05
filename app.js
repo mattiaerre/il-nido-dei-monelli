92e3591cf86c05cc4f10820b281e7f002794e2a9
mattia richetto <mattia.richetto@gmail.com>
2016-06-05 20:56:12 +0100

    add eits [WIP]

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var contacts = require('./routes/contacts');
 var photogallery = require('./routes/photogallery');
+var eits = require('./routes/eits');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/contacts', contacts);
 app.use('/photogallery', photogallery);
+app.use('/eits', eits);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ public/javascripts/eits.js
@@ -0,0 +1,4 @@
+function openPopup() {
+  var newwindow = window.open('../eits/login', 'eits', 'height=415,width=550,scrollbars=0,status=0');
+  if (window.focus) { newwindow.focus(); }
+}
